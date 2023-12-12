941222bdefb1d1545a3004ac31bbbe9435ff1779
MT <04x0xx@gmail.com>
2023-12-11 20:09:54 -0400

    Added functionality to get stats function

--- chess/analytics.py
+++ chess/analytics.py
@@ -9,14 +9,66 @@ Index(['url', 'pgn', 'time_control', 'end_time', 'rated', 'accuracies', 'tcn',
       dtype='object')
 """
 
-def read_stats(filename: str):
+def get_file_data(filename: str) -> pd.DataFrame:
     """
-    Function for reading the stats of a player from a stats file
+    Function for getting the dataframe from a file
     """
 
     with open(filename, "rb") as f:
         df = pickle.load(f)
         f.close()
+
+    return df
+
+
+def get_game_subset(df, game_time_class: str, game_rules: str = "chess"):
+    """
+    Function for getting a subset of games with the specifications
+        (df.time_control == game_time) & \
+    """
+
+    return df.query("(time_class==@game_time_class) & (rules==@game_rules)")
+
+
+def read_stats(username: str, filename: str):
+    """
+    Function for reading the stats of a player from a stats file
+    """
+
+    df = get_file_data(filename)
+    analytics_df = get_game_subset(df, "bullet", "chess")
+
+    
+    print(pd.DataFrame.from_dict(analytics_df.white, orient="index", columns=["username", "result"]))
+    exit()
+
+    results = pd.concat([
+        pd.DataFrame.from_dict(analytics_df.black),
+        pd.DataFrame.from_dict(analytics_df.white)
+    ])
+
+    print(results)
+    exit()
+
+    games_as_black = [1 if i["username"].lower()==username else 0 for i in analytics_df.black]
+    games_as_white = [1 if i==0 else 0 for i in games_as_black]
+
+    "win timeout resigned checkmated agreed repetition"
+    print(games_as_black)
+    # print(games_as_white)
+    """
+    df_control_mode_subset = df[df
+    games_as_black = df.loc([])
+    games_as_white = ""
+    game_elo_accuracies = ""
+    performance_over_time = ""
+
+    analytics_df = pd.DataFrame(
+            df.accuracies
+    )
+    """
+
+    df = get_game_subset(df, "bullet", "chess")
 
     print(df.head())
     print(df.columns)
